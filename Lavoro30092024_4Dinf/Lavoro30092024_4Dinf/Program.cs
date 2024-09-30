df6204104dd6442cb7f32469ff2af379c0dba75f
GioIta07 <153987927+GioIta07@users.noreply.github.com>
2024-09-30 12:28:41 +0200

    lavoro aggiunto

---
+++ Lavoro30092024_4Dinf/Lavoro30092024_4Dinf/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("gestione bliblioteca");
+
+Console.WriteLine("inserimento libri");
+
+bool continua= true;
+while (continua)
+{
+    Console.WriteLine("inserisci titolo:");
+    string titolo = Console.ReadLine();
+    Console.WriteLine("inserisci autore:");
+    string autore = Console.ReadLine();
+    Console.WriteLine("inserisci numero pagine:");
+    int pagine = int.Parse(Console.ReadLine());
+    Console.WriteLine("inserisci prezzo:");
+    decimal prezzo = decimal.Parse(Console.ReadLine());
+
+    Console.WriteLine("inserisci se vuole continuare:");
+        string risposta = Console.ReadLine();
+    if (risposta == "n") continua= false ;
+}
+
+
